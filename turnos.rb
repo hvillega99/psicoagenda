8fd4c7aefb4cab05955c65975a2031d4eaf7f804
hvillega99 <hvillega@espol.edu.ec>
2021-08-22 14:03:09 -0500

    Crud de turnos listo

---
+++ turnos.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'json'
+require 'mysql2'
+
+client = Mysql2::Client.new(
+    :host => "b6joa72sftrldpb59ymx-mysql.services.clever-cloud.com", 
+    :username => "utx94vo4wamo01ew", 
+    :password => "eZf9Ncc4YyeQ9uWBRgAq", 
+    :database => "b6joa72sftrldpb59ymx"
+)
+
+before do
+    content_type :json
+end
+
+get '/turnos' do
+    @result = client.query("SELECT * FROM turnos", :symbolize_keys => true).each
+    erb :turnos
+end
+
+get '/turnos/:id' do
+    @result = client.query("SELECT * FROM turnos WHERE id='#{params[:id]}'", :symbolize_keys => true).each
+    erb :turnos
+end
+
+get '/turnos/disponibles/:idPsicologo' do
+    @result = client.query("SELECT * FROM turnos where estado='disponible' and idPsicologo='#{params[:idPsicologo]}'", :symbolize_keys => true).each
+    erb :turnos
+end
+
+post '/turnos' do
+    data = JSON.parse request.body.read
+    client.query("INSERT INTO turnos (fecha,hora,estado,idPsicologo) VALUES ('#{data['fecha']}','#{data['hora']}','#{data['estado']}','#{data['idPsicologo']}')")
+    @result = client.query("SELECT * FROM turnos ORDER BY id DESC LIMIT 1", :symbolize_keys => true).each
+    erb :turnos
+end
+
+put '/turnos/:id' do
+    data = JSON.parse request.body.read
+    client.query("UPDATE turnos SET fecha = '#{data['fecha']}', hora = '#{data['hora']}', estado = '#{data['estado']}', idPsicologo = '#{data['idPsicologo']}' WHERE id='#{params[:id]}'")
+    @result = client.query("SELECT * FROM turnos WHERE id='#{params[:id]}'", :symbolize_keys => true).each
+    erb :turnos
+end
