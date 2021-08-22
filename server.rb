0e2461a45aeac5b4f3dda092dc8f636f2cc91ea1
hvillega99 <hvillega@espol.edu.ec>
2021-08-21 22:52:18 -0500

    Conexión con la base de datos establecida

--- server.rb
+++ server.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
 require 'json'
+require 'mysql2'
+
+client = Mysql2::Client.new(:host => "b6joa72sftrldpb59ymx-mysql.services.clever-cloud.com", :username => "utx94vo4wamo01ew", :password => "eZf9Ncc4YyeQ9uWBRgAq", :database => "b6joa72sftrldpb59ymx")
+
+client.query("SELECT * FROM pacientes", :symbolize_keys => true).each do |row|
+
+    puts "row: #{row}"
+    
+end
 
 before do
     content_type :json
@@ -14,5 +23,3 @@ get '/saludo/:nombre' do
     @message = "Hola #{params[:nombre]}"
     erb :result
 end
-
-get '/observaciones/:paciente'
