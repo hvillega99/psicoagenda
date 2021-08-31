13969c541a1c9cafd3afb33bdcd33c05a58fee3a
Carlos <carlosqa73@hotmail.com>
2021-08-31 11:48:43 -0500

    js de Psicologo, se muestran las citas

--- frontend/js/psicologo.js
+++ frontend/js/psicologo.js
@@ -1,10 +1,124 @@
-window.addEventListener("load", function(event) {
+
+function borrarL() {
+    localStorage.removeItem('tipo');
+    localStorage.removeItem('id');
+    window.location.href='login.html';
+  }
+
+const getInfo = async () => {
+    const uri = `http://127.0.0.1:4567/psicologos/${localStorage.getItem("id")}`;
+    const response = await fetch(uri);
+    const result = response.json();
+    return result;
+}
+
+const getCitas = async () => {
+    const uri = `http://127.0.0.1:4567/citasPorPsicologo/${localStorage.getItem("id")}`;
+    const response = await fetch(uri);
+    const result = response.json();
+    return result;
+}
+
+const showCitas = (citas) => {
+    const citasAnteriores = citas.filter(cita => cita.estado == 'Finalizada');
+    const divHistorial = document.getElementById('historial');
+
+    if (citasAnteriores.length > 0){
+        let table = `<table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Paciente</th>
+                                <th scope="col">Fecha</th>
+                                <th scope="col">Hora</th>
+                            </tr>
+                        </thead>
+                        <tbody>`;
+
+        citasAnteriores.forEach(element => {
+            table +=`<tr>
+                        <td>${element.nombreCompleto}</td>
+                        <td>${element.fecha}</td>
+                        <td>${element.hora}</td>
+                    </tr>`;
+        });
+
+        table += `</tbody></table>`;
+
+        divHistorial.innerHTML ='<h3 class="text-center">Historial de atenciones</h3>' + table;
+    }else{
+        divHistorial.innerHTML = '<h3 class="text-center">No hay información para mostrar</h3>';
+    }
+    
+}
+
+
+window.addEventListener("load", async(event) => {
     const tipo = localStorage.getItem('tipo');
     if(tipo){
-        if(tipo=='paciente'){
+        if(tipo!='psicologo'){
             window.location.href='dashboardPaciente.html';
         }
-    }else{
+        else{
+            const psicologo = await getInfo();
+            document.getElementById('saludo').textContent = `Bienvenido ${psicologo[0].nombreCompleto}`;
+
+            const citas = await getCitas();
+
+            showCitas(citas);
+
+            const result = citas.find(item => item.estado == 'No iniciada')
+
+            if(result){
+                document.getElementById('status').textContent = 'Usted tiene una cita agendada';
+
+                const divInfo = document.getElementById('cita-info');
+                divInfo.innerHTML = `<div class="card text-center">
+                <div class="card-header">
+                  Información de la cita
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Paciente: ${result.nombreCompleto}</h5>
+                  <p class="card-text">
+                    Fecha: ${result.fecha}
+                    Hora: ${result.hora}
+                  </p>
+                  <button class="btn btn-danger">Cancelar</button>
+                </div>
+              </div>`
+
+            }else{
+                document.getElementById('status').textContent = 'Actualmente no tiene una cita agendada';
+            }
+            
+        }
+    }
+    else{
         window.location.href='login.html';
     }
+});
+
+const setInvisible = () => {
+    const elements = document.getElementsByClassName('option-panel');
+
+    for(let i=0; i<elements.length; i++){
+      elements[i].style.display='none';
+    }
+}
+
+document.getElementById('tab-inicio')
+.addEventListener('click', e =>{
+  setInvisible();
+  document.getElementById('inicio').style.display='block';
+});
+
+document.getElementById('tab-agendar')
+.addEventListener('click', e =>{
+  setInvisible();
+  document.getElementById('agendar').style.display='block';
+});
+
+document.getElementById('tab-historial')
+.addEventListener('click', e =>{
+  setInvisible();
+  document.getElementById('historial').style.display='block';
 });
