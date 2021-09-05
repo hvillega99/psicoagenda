9b3176ef562ec30bc8fdf72f0bdd01537a03d82c
hvillega99 <hvillega@espol.edu.ec>
2021-09-05 00:44:50 -0500

    Funciones de agendar y cancelar cita implementadas en el dashboard del paciente

--- frontend/js/paciente.js
+++ frontend/js/paciente.js
@@ -1,10 +1,60 @@
+let tieneCita = false;
+
+const showAlertMessage = (message, tipoAlerta) =>{
+  const divMessage = document.getElementById('message');
+  divMessage.innerHTML =`<div class="alert alert-${tipoAlerta}" role="alert">${message}</div>`;
+  setTimeout(()=>{
+      divMessage.innerHTML = '';
+  },5000);
+}
+
 function borrarL() {
   localStorage.removeItem('tipo');
   localStorage.removeItem('id');
   window.location.href='login.html';
 }
 
+const agendarCita = async (id) => {
+  const newCita = {
+    estado: 'No iniciada',
+    idPaciente: localStorage.getItem("id"),
+    idTurno: id
+  };
 
+  const url = `http://127.0.0.1:4567/citas`;
+  await fetch(url, {
+    method: 'POST',
+    mode: 'no-cors', 
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newCita)
+  })
+
+  const citas = await getCitas();
+  renderStatusCita(citas);
+  renderTurnos(localStorage.getItem('idPsicologo'));
+
+  showAlertMessage('Cita agendada exitosamente', 'success');
+}
+
+const cancelarCita = async (id) => {
+  if(confirm("¿Está seguro?")){
+    const url = `http://127.0.0.1:4567/citas/${id}`;
+    await fetch(url, {
+      method: 'DELETE',
+      mode: 'same-origin', 
+      headers: {
+          'Content-Type': 'application/json'
+      }
+    })
+
+    const citas = await getCitas();
+    renderStatusCita(citas);
+    tieneCita = false;
+    showAlertMessage('La cita ha sido cancelada', 'warning');
+  }
+}
 
 const getInfo = async () => {
     const uri = `http://127.0.0.1:4567/pacientes/${localStorage.getItem("id")}`;
@@ -20,7 +70,13 @@ const getCitas = async () =>{
     return result;
 }
 
-const showCitasAnteriores = (citas) => {
+const getPsicologos = async () => {
+  const response = await fetch('http://127.0.0.1:4567/psicologos');
+  const psicologos = response.json();
+  return psicologos;
+}
+
+const renderHistorialCitas = (citas) => {
     const citasAnteriores = citas.filter(cita => cita.estado == 'Finalizada');
     const divHistorial = document.getElementById('historial');
 
@@ -52,6 +108,103 @@ const showCitasAnteriores = (citas) => {
     
 }
 
+const renderDatos = (paciente) => {
+  document.getElementById('saludo').textContent = `¡Hola ${paciente.nombreCompleto}!`;
+  document.getElementById('nombre').textContent = `${paciente.nombreCompleto}`;
+
+  document.getElementById('perfilNombre').textContent = `Nombre: ${paciente.nombreCompleto}`;
+  document.getElementById('perfilCedula').textContent = `Cédula: ${paciente.cedula}`;
+  document.getElementById('perfilMail').textContent = `Email: ${paciente.email}`;
+}
+
+const renderStatusCita = (citas) => {
+  const result = citas.find(item => item.estado == 'No iniciada');
+
+  const divStatus = document.getElementById('status');
+  const divInfo = document.getElementById('cita-info');
+
+  if(result){
+      tieneCita = true;
+      divStatus.textContent = 'Usted tiene una cita agendada';
+      divInfo.innerHTML = `<div class="card text-center" id="cita-${result.id}">
+      <div class="card-header">
+        Información de la cita
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Psicólogo: ${result.nombreCompleto}</h5>
+        <p class="card-text">
+          Fecha: ${result.fecha}
+          Hora: ${result.hora}
+        </p>
+        <button class="btn btn-danger" onclick=cancelarCita(${result.id})>Cancelar</button>
+      </div>
+    </div>`
+
+  }else{
+    divStatus.textContent = 'Actualmente no tiene una cita agendada';
+    divInfo.innerHTML = `<img src="./img/sad.png" class="img-fluid w-25 h-25" alt="psi">`;
+  }
+}
+
+const filterTurnos = (arrayTurnos) => {
+  const fecha = new Date();
+  const now = `${fecha.getHours()}:${fecha.getMinutes()}`;
+  const dateItems = fecha.toLocaleDateString().split('/');
+  const today = `${dateItems[2]}-${dateItems[1].length < 2? '0'+dateItems[1]:dateItems[1]}-${dateItems[0].length < 2? '0'+dateItems[0]:dateItems[0]}`
+  
+  return arrayTurnos.filter(turno => {
+    if(turno.fecha > today){
+      return true;
+    }else if(turno.fecha == today){
+      return turno.hora > now;
+    }
+    return false
+  });
+}
+
+const renderTurnos = async (idPsicologo) => {
+  localStorage.setItem('idPsicologo', idPsicologo);
+  const response = await fetch(`http://127.0.0.1:4567/turnosDisponibles/${idPsicologo}`);
+  const allTurnos = await response.json();
+
+  const turnos = filterTurnos(allTurnos);
+
+  const divTurnos = document.getElementById('turnos-disponibles');
+  let elements = '';
+  turnos.forEach(turno => {
+    elements += `<div class="card card-body text-center" id="turno-${turno.id}">
+                    <p class="card-text">
+                      Fecha: ${turno.fecha}
+                    </p>
+                    <p class="card-text">
+                      Hora: ${turno.hora}
+                    </p>`;
+    if(!tieneCita){
+      elements += `<button type="button" class="btn btn-primary" onclick=agendarCita(${turno.id})>Agendar</button>`;
+    }
+
+    elements += '</div>';
+
+  })
+  divTurnos.innerHTML = elements;
+}
+
+const renderPsicologos = (psicologos) =>{
+  const divPsicologos = document.getElementById('psicologos')
+  let lista = `<div class="btn-group d-flex flex-column" 
+                role="group" aria-label="Basic radio toggle button group">`;
+
+  psicologos.forEach(item => {
+    lista += `<input type="radio" class="btn-check" name="btnradio" id="psicologo-${item.id}" 
+              autocomplete="off" cheked onclick=renderTurnos(${item.id})>
+              <label class="btn btn-outline-primary" for="psicologo-${item.id}">
+                ${item.nombreCompleto}
+              </label>`
+  })
+  
+  divPsicologos.innerHTML = lista + '</div>';
+}
+
 window.addEventListener("load", async (event) => {
     const tipo = localStorage.getItem('tipo');
     if(tipo){
@@ -59,43 +212,15 @@ window.addEventListener("load", async (event) => {
             window.location.href='dashboardPsicologo.html';
         }else{
             const paciente = await getInfo();
-            document.getElementById('saludo').textContent = `¡Hola ${paciente[0].nombreCompleto}!`;
-            document.getElementById('nombre').textContent = `${paciente[0].nombreCompleto}`;
-
-            //Datos personales---------
-            document.getElementById('perfilNombre').textContent = `Nombre: ${paciente[0].nombreCompleto}`;
-            document.getElementById('perfilCedula').textContent = `Cédula: ${paciente[0].cedula}`;
-            document.getElementById('perfilMail').textContent = `Email: ${paciente[0].email}`;
+            renderDatos(paciente[0]);
             
             const citas = await getCitas();
+            renderStatusCita(citas);
+            renderHistorialCitas(citas);
 
-            showCitasAnteriores(citas);
+            const psicologos = await getPsicologos();
+            renderPsicologos(psicologos);
 
-            const result = citas.find(item => item.estado == 'No iniciada')
-
-            const divStatus = document.getElementById('status');
-            const divInfo = document.getElementById('cita-info');
-
-            if(result){
-                divStatus.textContent = 'Usted tiene una cita agendada';
-                divInfo.innerHTML = `<div class="card text-center">
-                <div class="card-header">
-                  Información de la cita
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">Psicólogo: ${result.nombreCompleto}</h5>
-                  <p class="card-text">
-                    Fecha: ${result.fecha}
-                    Hora: ${result.hora}
-                  </p>
-                  <button class="btn btn-danger">Cancelar</button>
-                </div>
-              </div>`
-
-            }else{
-              divStatus.textContent = 'Actualmente no tiene una cita agendada';
-              divInfo.innerHTML = `<img src="./img/sad.png" class="img-fluid w-25 h-25" alt="psi">`;
-            }
         }
     }else{
         window.location.href='login.html';
